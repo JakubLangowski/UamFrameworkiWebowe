e586476b19b0cf620e0e4d55fc829ab5519bb3c2
Jakub Łangowski <jaklan@st.amu.edu.pl>
2021-10-21 12:40:20 +0200

    Tailwind Fix + Axios + .env

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,13 @@
+// https://stackoverflow.com/questions/63067555/how-to-make-an-import-shortcut-alias-in-create-react-app
+
+const path = require(`path`);
+
 module.exports = {
+    webpack: {
+        alias: {
+            '@assets': path.join(path.resolve(__dirname, './src', 'assets')),
+        }
+    },
     style: {
         postcss: {
             plugins: [
